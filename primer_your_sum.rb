aec08039be7484fa25020807a13560a8cddcadd0
David O'Keefe <davidpokeefe3@gmail.com>
2013-11-17 15:20:05 -0500

    first commit

---
+++ primer_your_sum.rb
@@ -0,0 +1,25 @@
+# Problem Statement
+# Given a custom class MyArray, 
+# write a 'sum' method that takes a block parameter.
+
+class MyArray
+  attr_reader :array
+
+  def initialize(array)
+    @array = array
+  end
+
+  def sum(initial_value = 0)
+    if block_given?
+    	@array.inject(initial_value) { |sum, number| sum += yield(number) }
+    else
+        @array.inject(initial_value) { |sum, number| sum += number }
+    end
+  end
+end
+
+# specs:
+# my_array = MyArray.new([1, 2, 3]) 
+# p my_array.sum == 6
+# p my_array.sum(10) == 16
+# p my_array.sum(0) {|n| n ** 2 } == 14
---
+++ readme.md
@@ -0,0 +1,3 @@
+####[Ruby Monk Exercises] (http://www.rubymonk.com)  
+
+These are completed exercises from the Ruby Monk Primer.
